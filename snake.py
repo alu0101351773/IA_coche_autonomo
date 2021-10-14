c71cee40f31e0ccf77a0af3e87744dd5558625c2
jorge <alu0101351773@ull.edu.es>
2021-10-14 19:47:50 +0100

    Interfaz arreglada: sobredimensionable

--- snake.py
+++ snake.py
@@ -10,56 +10,66 @@ from py_matrix import Matriz
 
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
+RED = (255, 0, 0)
 
 INITIAL_WINDOW_HEIGHT = 900
 INITIAL_WINDOW_WIDTH = 900
 
-matriz_a = Matriz(3, 3)
+matriz_a = Matriz(2, 2)
 
-block_height = 0
-block_width = 0
+# block_height = INITIAL_WINDOW_HEIGHT // matriz_a.get_height()
+# block_width = INITIAL_WINDOW_WIDTH // matriz_a.get_width()
+
 
 def main():
     global SCREEN, CLOCK
     pygame.init()
     SCREEN = pygame.display.set_mode((INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption("Ventana de coche autonomo")
+    CLOCK = pygame.time.Clock()
     
 
     while True:
         SCREEN.fill(WHITE)
 
-        global block_height
-        global block_width
-
-        block_height = SCREEN.get_height() // matriz_a.get_height()
-        block_width = SCREEN.get_width() // matriz_a.get_width()
-
-        print(block_height, block_width)
-
         drawGrid()
+
+        pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             
+            # Innecesario, pero lo dejo por si en un futuro
             if event.type == pygame.VIDEORESIZE:
                 SCREEN = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
 
-        pygame.display.update()
-
+        
 
 def drawGrid():
 
-    global block_height
-    global block_width
+    block_height = SCREEN.get_height() // matriz_a.get_height()
+    block_width = SCREEN.get_width() // matriz_a.get_width()
 
     # blockSize = 5 #Set the size of the grid block
-    for x in range(0, SCREEN.get_width(), block_height):
-        for y in range(0, SCREEN.get_height(), block_width):
-            rect = pygame.Rect(x, y, block_height, block_width)
-            pygame.draw.rect(SCREEN, BLACK, rect, 1)
+    for x in range(0, SCREEN.get_width(), block_width):
 
-# https://www.programiz.com/python-programming/matrix
+        # Auxiliar
+        auxiliar_x = (x // block_width) % matriz_a.get_width()
+
+        for y in range(0, SCREEN.get_height(), block_height):
+            
+            # Auxiliar
+            auxiliar_y = (y // block_height) % matriz_a.get_height()
+
+            rect = pygame.Rect(x, y, block_width, block_height)
+
+            print(auxiliar_x, auxiliar_y)
+
+            if (auxiliar_x % 2 == 0) & (auxiliar_y % 2 == 0):
+                pygame.draw.rect(SCREEN, RED, rect, 0)
+            else:
+                pygame.draw.rect(SCREEN, BLACK, rect, 1)
+
 main()
-
