2865e04f2574530e25ad52f8d0e7496417130174
Marc Carbonell <72404949+alu0101323282@users.noreply.github.com>
2021-10-22 18:56:53 +0100

    Add files via upload

--- aestrella.py
+++ aestrella.py
@@ -3,8 +3,8 @@ class Mapa:
     def __init__(self, fil, col, lista_obst, ori =[0,0], dest = [0,0]):
         self.alto = fil
         self.ancho = col
-        self.origen = ori
-        self.destino = dest
+        self.origen = Nodo(dest,ori)
+        self.destino = Nodo(dest,dest)
         self.obstaculos = lista_obst
 
     def casilla_libre(self, fila,columna):
@@ -13,12 +13,11 @@ class Mapa:
             return False
         if columna < 0 or columna > self.ancho:
             return False
-        for i in range(len(self.obstaculos)):
-            if self.obstaculos[i] == pos:
+        for i in self.obstaculos:
+            if i == pos:
                 return False
         return True
     
-
 
 class Nodo:
     def __init__(self, pos_f, pos=[0, 0], padre=None):
@@ -34,6 +33,62 @@ class Nodo:
         # Peso del nodo
         self.f = self.g + self.h
 
+
+class Aestrella:
+    def __init__(self, map):
+        self.abiertos = []
+        self.cerrados = []
+        self.mapa = map
+        self.abiertos.append(self.mapa.origen)
+
+    def algoritmo(self):
+        while nodo_menor(self.abiertos).pos != self.mapa.destino.pos:
+            menor = nodo_menor(self.abiertos)
+            self.abiertos.remove(menor)
+            self.gen_hijos(menor)
+            self.cerrados.append(menor)
+        return nodo_menor(self.abiertos)
+
+    def imprimir_camino(self,nodo):
+        camino = []
+        while True:
+            camino.append(nodo.pos)
+            if nodo.padre == None:
+                break
+            nodo = nodo.padre
+        for i in reversed(camino):
+            print(i)
+    
+    def gen_hijos(self, padre):
+        # Arriba
+        if self.mapa.casilla_libre(padre.pos[0] - 1,padre.pos[1]):
+            hijo = Nodo(self.mapa.destino.pos,[padre.pos[0] - 1,padre.pos[1]],padre)
+            self.meter_en_lista(hijo)  
+        # Abajo
+        if self.mapa.casilla_libre(padre.pos[0] + 1,padre.pos[1]):
+            hijo = Nodo(self.mapa.destino.pos,[padre.pos[0] + 1,padre.pos[1]],padre)
+            self.meter_en_lista(hijo)
+        #Izquierda
+        if self.mapa.casilla_libre(padre.pos[0],padre.pos[1] - 1):
+            hijo = Nodo(self.mapa.destino.pos,[padre.pos[0],padre.pos[1] - 1],padre)
+            self.meter_en_lista(hijo)
+        #Derecha
+        if self.mapa.casilla_libre(padre.pos[0],padre.pos[1] + 1):
+            hijo = Nodo(self.mapa.destino.pos,[padre.pos[0],padre.pos[1] + 1],padre)
+            self.meter_en_lista(hijo)
+   
+    def meter_en_lista(self, nodo):
+        nodo_igual = 0
+        for i in self.abiertos:
+            if nodo.pos == i.pos:
+                nodo_igual = i
+        if nodo_igual == 0:
+            self.abiertos.append(nodo)
+        elif nodo.f < nodo_igual.f:
+            self.abiertos.remove(nodo_igual)
+            self.abiertos.append(nodo)
+
+
 # Distancia entre dos puntos.
 def distancia(a, b):
     # heurística de Manhattan
@@ -46,45 +101,16 @@ def nodo_menor(lista):
             menor = lista[i]
     return menor
 
-
-def gen_hijos(map, lista, padre):
-    # Arriba
-    if map.casilla_libre(padre.pos[0] - 1,padre.pos[1]):
-        lista.append(Nodo(map.destino,[padre.pos[0] - 1,padre.pos[1]],padre))
-    # Abajo
-    if map.casilla_libre(padre.pos[0] + 1,padre.pos[1]):
-        lista.append(Nodo(map.destino,[padre.pos[0] + 1,padre.pos[1]],padre))
-    #Izquierda
-    if map.casilla_libre(padre.pos[0],padre.pos[1] - 1):
-        lista.append(Nodo(map.destino,[padre.pos[0],padre.pos[1] - 1],padre))
-    #Derecha
-    if map.casilla_libre(padre.pos[0],padre.pos[1] + 1):
-        lista.append(Nodo(map.destino,[padre.pos[0],padre.pos[1] + 1],padre))
-
 def main():
     fil = 5
     col = 5
     lista_obst = [[0,1],[1,1],[1,3],[1,4],[3,2],[3,4],[4,2]]
     ori = [0,0]
     dest = [4,4]
+
     map = Mapa(fil,col,lista_obst,ori,dest)
-    abiertos = []
-    cerrados = []
-    abiertos.append(Nodo(map.destino,map.origen))
-    while nodo_menor(abiertos).pos != map.destino:
-        menor = nodo_menor(abiertos)
-        abiertos.remove(menor)
-        gen_hijos(map, abiertos, menor)
-        cerrados.append(menor)
-    casilla = nodo_menor(abiertos)
-    camino = []
-    while True:
-        camino.append(casilla.pos)
-        if casilla.padre == None:
-            break
-        casilla = casilla.padre
-    for i in range(len(camino)):
-        print(camino[i], " ")
+    astar = Aestrella(map)
+    astar.imprimir_camino(astar.algoritmo())
 
 main()
 
